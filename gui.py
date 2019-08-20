9d47381c80ae6c897b47d270d1823877c382c9ad
Pavel Savchuk <psavchuk@beltechsoftware.com>
2019-08-20 17:48:50 +0300

    Add tree generation

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3.6
 
+import math
 import random
 import wx
 
@@ -36,12 +37,51 @@ class MainFrame(wx.Frame):
         value = (int)(random.random() * 100_000)
         self.seedInput.SetValue((str)(value))
 
+    def getSeed(self):
+        seed = self.seedInput.GetValue()
+        while len(seed) == 0:
+            self.randomizeSeed()
+            seed = self.seedInput.GetValue()
+
+        return seed
+
     def generate(self, event = None):
+        random.seed(self.getSeed())
+
         w, h = self.panel1.GetClientSize()
+        x = w / 2;
+        y = h;
+        angle = -math.pi / 2
+        length = (int)(y * random.randint(20, 30) / 100.0)
+        thickness = 5
 
         dc = wx.ClientDC(self.panel1)
         dc.Clear()
-        dc.DrawLine(0, 0, w, h)
+        
+        self.renderTree(dc, x, y, length, thickness, angle)
+
+    def renderTree(self, dc, x, y, length, thickness, angle):
+        pen = dc.GetPen()
+        pen.SetWidth(thickness)
+
+        dc.SetPen(pen)
+
+        x1 = (int)(x + length * math.cos(angle))
+        y1 = (int)(y + length * math.sin(angle))
+
+        dc.DrawLine(x, y, x1, y1)
+
+        for _branch in [1] * random.randrange(2,6):
+            scale = random.randint(60, 80) / 100.0
+            new_length = length * scale
+            new_thickness = thickness * scale
+            
+            if new_length > 4:
+                new_angle = angle + random.randint(-61, 60) * -math.pi / 180.0
+
+                self.renderTree(dc, x1, y1, new_length, new_thickness, new_angle)
+
+
 
 class MyApp(wx.App):
     def OnInit(self):
